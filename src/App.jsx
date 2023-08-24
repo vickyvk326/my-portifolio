3ac4b42cec3f7c154e41c73053c56bd7be412f8a
Vignesh Ravichandran <114295235+vickyvk326@users.noreply.github.com>
2023-08-24 19:36:43 +0530

    BorwserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import { Component } from "react";
 import Header from "./components/Header";
-import Intro from "./components/Intro";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainContainer from "./components/MainContainer";
 export default class App extends Component {
   render() {
     return (
       <div className="">
-        <Router>
+        <BrowserRouter>
           <Header />
           <Routes>
             <Route path="/" element={<MainContainer />} />
-            <Route path="/works" element={<Intro />} />
-            <Route path="/blog" element={<Intro />} />
-            <Route path="/contact" element={<Intro />} />
+            <Route path="/works" element={<h1>works</h1>} />
+            <Route path="/blog" element={<h1>blog</h1>} />
+            <Route path="/contact" element={<h1>contact</h1>} />
           </Routes>
-        </Router>
+        </BrowserRouter>
         <Footer />
       </div>
     );
