9b0e8a4c0fdd4b72dc0b0ea8868b98745d4a05f2
Vignesh Ravichandran <114295235+vickyvk326@users.noreply.github.com>
2023-08-24 19:48:00 +0530

    try hashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,14 @@ import MainContainer from "./components/MainContainer";
 export default class App extends Component {
   render() {
     return (
-      <div className="">
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<MainContainer />} />
-            <Route path="/works" element={<h1>works</h1>} />
-            <Route path="/blog" element={<h1>blog</h1>} />
-            <Route path="/contact" element={<h1>contact</h1>} />
-          </Routes>
-        </BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainContainer />} />
+          <Route path="/works" element={<h1>works</h1>} />
+          <Route path="/blog" element={<h1>blog</h1>} />
+          <Route path="/contact" element={<h1>contact</h1>} />
+        </Routes>
         <Footer />
       </div>
     );
